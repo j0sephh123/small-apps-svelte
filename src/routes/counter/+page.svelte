0a4534bb702c35e4d4e49ab79a7625af2ae757e1
j0sephh123 <iossif123gg@gmail.com>
2023-01-02 10:01:00 +0200

    add routing to svelte

---
+++ src/routes/counter/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  let count = 0;
+  $: className = count === 0 ? null : (count > 0 ? "success" : "failure");
+
+  function handleDecrement() {
+    count -= 1;
+  }
+  function handleIncrement() {
+    count += 1;
+  }
+  function handleReset() {
+    count = 0;
+  }
+</script>
+
+<div id="app">
+  <div class="content">
+    <button on:click={handleDecrement}>-</button>
+    <div class={className} id="counter">{count}</div>
+    <button on:click={handleIncrement}>+</button>
+  </div>
+  <button on:click={handleReset}>Reset</button>
+</div>
